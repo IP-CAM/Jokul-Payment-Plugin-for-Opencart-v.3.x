d26c61da4b69118802dc9734f7f6532d52bd5551
Arief R Ramadhan <arief.rizky@doku.com>
2021-03-24 20:11:08 +0700

    Update configuration wordings, update default value Client ID and Secret Key, update DOKU Jokul logo

--- admin/language/en-gb/extension/payment/doku.php
+++ admin/language/en-gb/extension/payment/doku.php
@@ -12,12 +12,12 @@
  */
 
 // Heading
-$_['heading_title']                = 'DOKU';
+$_['heading_title']                = 'Jokul - General Configuration';
                                    
 // Text                            
 $_['text_payment']                 = 'Payment';
 $_['text_success']                 = 'Success: You have modified DOKU account details!';
-$_['text_doku']        = '<a onclick="window.open(\'https://www.doku.com\');"><img src="view/image/payment/doku.png" alt="DOKU" title="DOKU" style="border: 1px solid #EEEEEE;" /><br /></a>';
+$_['text_doku']        = '<a onclick="window.open(\'https://jokul.doku.com\');"><img src="view/image/payment/doku.png" alt="DOKU" title="DOKU" /><br /></a>';
 $_['text_edit']              	   = 'Edit configuration';
 $_['text_extension']	    		= 'Extensions';
 // Parameter                       
@@ -26,9 +26,9 @@ $_['opencart_params']              = 'Opencart Server Parameter';
 $_['paymentchannel_params']        = 'Payment Channel Parameter';
                                    
 // Entry                           
-$_['entry_server_set']             		= 'Server Set :';
-$_['entry_mallid']          			= 'Client ID :';
-$_['entry_shared']          			= 'Client Shared Key :';
+$_['entry_server_set']             		= 'Environment:';
+$_['entry_mallid']          			= 'Client ID:';
+$_['entry_shared']          			= 'Secret Key:';
 $_['entry_review_edu']             		= 'Use EDU Services ?';
 $_['entry_identify']               		= 'Use Identify ?';
 $_['entry_tokenization']               	= 'Use Tokenization Credit Card ?';
@@ -54,8 +54,8 @@ $_['paymentchannel_dokualfa']      = 'DOKU Alfa channel:';
 $_['error_permission']             = 'Warning: You do not have permission to modify payment DOKU!';
 $_['error_server_set']             = 'Server target has to be set!';
 $_['error_companyid']              = 'Warning: You do not have permission to modify payment DOKU!';
-$_['error_mallid']                 = 'Mall ID Required!';
-$_['error_shared']                 = 'Shared Key Required!';
+$_['error_mallid']                 = 'Client ID Required!';
+$_['error_shared']                 = 'Secret Key Required!';
 $_['error_expired_time']           = 'Expired Time Status Required!';
 $_['error_check_key']              = 'Check Key Required!';
 $_['error_payment_method']         = 'Payment Method Name Required!';
@@ -97,7 +97,7 @@ if (!in_array($_SERVER['SERVER_PORT'], [80, 443])) {
 }
 
 $myservername                = $_SERVER['SERVER_NAME'] .$port. $serverpath;
-$_['url_title']              = 'URL to be set on DOKU System';
+$_['url_title']              = 'Set this URL to your Jokul Back Office';
 $_['url_notify']             = $myserverprotocol.'://'.$myservername.'/index.php?route=extension/payment/doku/notify';
 
 ?>
